b067e4767cf35a0377ae6bf6166e09a01516a74c
Dalhen <hardelay.clement@protonmail.com>
2022-12-09 15:54:49 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,3 +37,7 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.text("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+fruit_choice_add = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('The user entered ', fruit_choice_add)
+fruit_choice_add_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice_add)
